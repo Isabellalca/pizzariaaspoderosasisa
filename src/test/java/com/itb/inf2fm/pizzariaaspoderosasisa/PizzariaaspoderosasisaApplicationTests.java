d58754b78007731596a82bc74c281bf738bebf86
Isabellalca <rm99734@estudante.fieb.edu.br>
2026-05-28 08:32:48 -0300

    Criado ambiente dev

---
+++ src/test/java/com/itb/inf2fm/pizzariaaspoderosasisa/PizzariaaspoderosasisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2fm.pizzariaaspoderosasisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaaspoderosasisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
